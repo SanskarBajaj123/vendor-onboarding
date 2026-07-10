15149f628d70f6c7372f4f5585bb96e37c580460
SanskarBajaj123 <bsanskar123@gmail.com>
2026-07-10 13:40:06 +0530

    Remove unused Card import from AuthPage

--- web/src/pages/AuthPage.tsx
+++ web/src/pages/AuthPage.tsx
@@ -3,7 +3,6 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { supabase } from "../lib/supabase";
 import { api } from "../lib/api";
-import { Card } from "../components/ui/Card";
 import { Button } from "../components/ui/Button";
 import { Field, SelectField, SectionLabel } from "../components/ui/Field";
 import { LiveRunView, type RunStage } from "../components/ui/LiveRunView";
